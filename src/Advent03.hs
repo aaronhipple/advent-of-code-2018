40a6ac593983f8d383711fb96efa895b34b26276
Aaron <ahipple@gmail.com>
2018-12-05 20:17:54 -0700

    Implement Day 3 part 1
    Counting *every inch* of *every claim*? Not great. Does the trick,
    though.

--- src/Advent03.hs
+++ src/Advent03.hs
@@ -1,9 +1,74 @@
 module Advent03
   ( exec_03
+  , Claim(..)
+  , parseClaim
+  , claimCoordinates
+  , claimsPerCoordinate
   ) where
+
+import           Data.Char
+import qualified Data.Map.Strict              as Map
+import           Data.Maybe
+import           Text.ParserCombinators.ReadP
+
+data Claim =
+  Claim Int
+        (Int, Int)
+        (Int, Int)
+  deriving (Show, Eq)
 
 exec_03 :: IO ()
 exec_03 = do
   putStrLn "*********"
   putStrLn "* Day 3 *"
   putStrLn "*********"
+  claims <-
+    fmap (map (fromJust . parseClaim) . lines) $ readFile "resources/03.txt"
+  putStrLn $
+    "Overlapping Claim Inches: " ++
+    show (length $ filter (> 1) $ Map.elems $ claimsPerCoordinate claims)
+
+type ClaimMap = Map.Map (Int, Int) Int
+
+claimsPerCoordinate :: [Claim] -> ClaimMap
+claimsPerCoordinate claims = claimMap
+  where
+    claimMap = foldl count Map.empty (concatMap claimCoordinates claims)
+    count :: ClaimMap -> (Int, Int) -> ClaimMap
+    count m coords = Map.insertWith (+) coords 1 m
+
+claimCoordinates :: Claim -> [(Int, Int)]
+claimCoordinates (Claim _ (x, y) (w, h)) =
+  [(x', y') | x' <- [x .. x + w - 1], y' <- [y .. y + h - 1]]
+
+parseClaim :: String -> Maybe Claim
+parseClaim str =
+  case parsed of
+    Nothing                   -> Nothing
+    Just (id, (x, y), (w, h)) -> Just $ Claim id (x, y) (w, h)
+  where
+    parsed = parseMaybe parser str
+    parser :: ReadP (Int, (Int, Int), (Int, Int))
+    parser = do
+      char '#'
+      id <- digits
+      skipSpaces
+      char '@'
+      skipSpaces
+      x <- digits
+      char ','
+      y <- digits
+      char ':'
+      skipSpaces
+      w <- digits
+      char 'x'
+      h <- digits
+      return (id, (x, y), (w, h))
+    digits :: ReadP Int
+    digits = fmap read $ munch1 isDigit
+
+parseMaybe :: ReadP a -> String -> Maybe a
+parseMaybe parser input =
+  case readP_to_S parser input of
+    []              -> Nothing
+    ((result, _):_) -> Just result
--- test/Advent03Test.hs
+++ test/Advent03Test.hs
@@ -2,8 +2,25 @@ module Advent03Test
   ( test_03
   ) where
 
-import           Advent03
+import           Advent03      (Claim (..), parseClaim)
+import           Control.Monad (forM_)
 import           Test.Hspec
+import           Text.Printf
 
 test_03 :: IO ()
-test_03 = hspec $ do describe "true" $ it "is true" $ True `shouldBe` True
+test_03 =
+  hspec $ do
+    describe "true" $ it "should be true" $ True `shouldBe` True
+    describe "parseClaim" $
+      forM_ testCases $ \(input, expected) ->
+        it (printf "should correctly parse claim `%s`" input) $
+        parseClaim input `shouldBe` expected
+
+testCases =
+  [ ("#1 @ 1,3: 4x4", Just (Claim 1 (1, 3) (4, 4)))
+  , ("#2 @ 3,1: 4x4", Just (Claim 2 (3, 1) (4, 4)))
+  , ("#3 @ 5,5: 2x2", Just (Claim 3 (5, 5) (2, 2)))
+  , ("INVALID CLAIM", Nothing)
+  , ("#532000 @ 52,51: 20x200", Just (Claim 532000 (52, 51) (20, 200)))
+  , ("#123@5,10:12x345", Just (Claim 123 (5, 10) (12, 345)))
+  ]
